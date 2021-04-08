bfbd4b6b470b2774dde7b4771522ffbf89707d74
megamp15 <47288803+megamp15@users.noreply.github.com>
2021-04-08 01:45:47 -0500

    Added late iframe load functionality

--- plots.js
+++ plots.js
@@ -1,13 +1,17 @@
-console.log(document.getElementsByTagName("iframe").length)
+// console.log(document.getElementsByTagName("iframe").length)
 
-iframe_load_counter = 0;
-function iframe_loaded(){
-    iframe_load_counter = iframe_load_counter +1;
-    console.log(iframe_load_counter);
-};
+// iframe_load_counter = 0;
+// function iframe_loaded(){
+//     iframe_load_counter = iframe_load_counter +1;
+//     console.log(iframe_load_counter);
+// };
 
-if (iframe_load_counter >= document.getElementsByTagName("iframe").length){
-    console.log('ALL LOADED');
-    // document.getElementsByTagName("body").display
+// if (iframe_load_counter >= document.getElementsByTagName("iframe").length){
+//     console.log('ALL LOADED');
+//     // document.getElementsByTagName("body").display
+// }
+
+function load_frame(sn_id, sn_type,plot_type){
+    var baseUrl = 'https://megamp15.github.io/Aggienova_Website/plots/';
+    document.getElementById(sn_id+'_'+plot_type).src = baseUrl+plot_type+'/'+sn_id+'_'+sn_type+'_series_'+plot_type+'_summaryPlot.html';
 }
-
